64ff354d842e6c56a94276634c35ccd85b90993b
Zhantore <753gg@mail.ru>
2020-09-07 11:56:02 +0600

    Таймер

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,13 @@ import modals from "./modules/modals";
 import tabs from "./modules/tabs";
 import forms from './modules/forms';
 import changeModalState from './modules/changeModalState';
+import timer from './modules/timer';
 
 window.addEventListener("DOMContentLoaded", () => {
   "use strict";
 
   let modalState = {};
+  let deadline = '2020-09-10';
 
   changeModalState(modalState);
   modals();
@@ -20,5 +22,6 @@ window.addEventListener("DOMContentLoaded", () => {
   );
   tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
   forms(modalState);
+  timer('.container1', deadline);
 
 });
